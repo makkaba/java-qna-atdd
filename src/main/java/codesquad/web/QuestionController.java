d98412e24c706b70dedae05fec6f3512707cc7a9
Jeff Chung <drifterz303@gmail.com>
2018-04-30 20:36:14 +0900

    질문 생성을 추가한다

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -1,4 +1,37 @@
 package codesquad.web;
 
+import codesquad.domain.Question;
+import codesquad.domain.User;
+import codesquad.dto.QuestionDto;
+import codesquad.security.LoginUser;
+import codesquad.service.QnaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/questions")
 public class QuestionController {
+    @Resource(name = "qnaService")
+    private QnaService qnaService;
+
+    @GetMapping("/{id}")
+    public String get(Model model, @PathVariable long id) {
+        model.addAttribute("question", qnaService.findById(id));
+        return "/qna/show";
+    }
+
+    @PostMapping("")
+    public String create(@LoginUser User loginUser, QuestionDto questionDto){
+        Question question = qnaService.create(loginUser, questionDto.toQuestion());
+        return String.format("redirect:%s", question.generateUrl());
+        //model.addAttribute("question", question);
+        //return "/qna/show";
+        //return "redirect:/qna/show";
+    }
 }
--- src/test/java/codesquad/web/QuestionAcceptanceTest.java
+++ src/test/java/codesquad/web/QuestionAcceptanceTest.java
@@ -1,6 +1,49 @@
 package codesquad.web;
 
+import codesquad.domain.QuestionRepository;
+import codesquad.domain.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
 
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class QuestionAcceptanceTest extends AcceptanceTest{
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    public void 질문Create(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("title", "the title");
+        params.add("contents", "hello");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+        ResponseEntity<String> response = basicAuthTemplate()
+                .postForEntity("/questions", request, String.class);
+        assertThat(response.getStatusCode(), is(HttpStatus.FOUND));
+
+    }
+    @Test
+    public void 질문Read(){
+
+    }
+    @Test
+    public void 질문Update(){
+
+    }
+    @Test
+    public void 질문Delete(){
+
+    }
 }
